5913f3be11db7830b422fc363c9452e5abe8bf13
Rishi Raj <rishiraj88+github@gmail.com>
2021-11-17 17:25:01 +0530

    A minor update in comment for better readability.

--- LeetCodeSolutions/src/LcMaxConsecutiveOnes.java
+++ LeetCodeSolutions/src/LcMaxConsecutiveOnes.java
@@ -22,9 +22,9 @@ public class LcMaxConsecutiveOnes {
 		maxCount = count;
 		
 		for (int i = 1; i < nums.length; i++) {
-			if (1 != nums[i]) {// ==0
+			if (1 != nums[i]) {// nums[i]==0
 				count = 0;
-			} else {// ==1
+			} else {// nums[i]==1
 				if (1 != nums[i - 1]) {
 					count = 1;
 				} else {
